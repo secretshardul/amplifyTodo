9579c8c3082d3a1aed0c9b6a050ec25ea15f2e6c
shardul <shardul.aeer@somaiya.edu>
2020-05-08 22:34:45 +0530

    initial boilerplate

---
+++ App.tsx
@@ -0,0 +1,24 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React from 'react';
+import {View, Text} from 'react-native';
+
+declare const global: {HermesInternal: null | {}};
+
+const App = () => {
+  return (
+    <View>
+      <Text>My app</Text>
+    </View>
+  );
+};
+
+export default App;
